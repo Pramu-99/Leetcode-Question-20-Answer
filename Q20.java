5e085a649e89133b8541af1d52f238dc4c6d3c1f
Pramu-99 <112867580+Pramu-99@users.noreply.github.com>
2023-08-13 13:35:57 +0530

    Add files via upload

---
+++ Q20.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+      Stack<Character> stack=new Stack<Character>();
+      int length=s.length();
+      for(int i=0;i<length;i++)
+      {
+        if(s.charAt(i)=='(')
+        {
+          stack.push(')');
+        }
+
+        else if(s.charAt(i)=='{')
+        {
+          stack.push('}');
+        }
+
+        else if(s.charAt(i)=='[')
+        {
+          stack.push(']');
+        }
+
+        else if(stack.isEmpty() || stack.pop()!=s.charAt(i))
+        {
+          return false;
+        }
+      }
+      return stack.isEmpty();
+}
+}
